c47ae4bcfffe2775a24c6b16deec7e9ff57d5dfc
Max <mfong415@gmail.com>
2023-04-29 11:17:24 -0700

    added gpt generated code

--- src/components/Orbit/OrbitFrame.tsx
+++ src/components/Orbit/OrbitFrame.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useRef } from "react";
+import "./Orbit.css";
 
-const OrbitFrame = () => {
+const OrbitFrame: React.FC = () => {
+  const div1Ref = useRef<HTMLDivElement>(null);
+  const div2Ref = useRef<HTMLDivElement>(null);
+  const div3Ref = useRef<HTMLDivElement>(null);
+
   return (
-    <div>
-      
+    <div className="orbit-container">
+      <div id="div1" className="orbit" ref={div1Ref} />
+      <div id="div2" className="orbit" ref={div2Ref} />
+      <div id="div3" className="orbit" ref={div3Ref} />
     </div>
-  )
-}
+  );
+};
 
-export default OrbitFrame
+export default OrbitFrame;
